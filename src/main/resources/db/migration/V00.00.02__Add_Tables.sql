9d87e8979a12f62e07534cd1c29579476953f4bb
Gela Bichashvili <bichashvili.gela@gmail.com>
2023-10-18 17:23:59 +0400

    flyway

---
+++ src/main/resources/db/migration/V00.00.02__Add_Tables.sql
@@ -0,0 +1,8 @@
+create table test(
+id serial primary key,
+iso_code_2 varchar(2) NOT NULL,
+iso_code_3 varchar(3) NOT NULL,
+name text,
+name_geo text,
+funny_name text
+);
---
+++ src/main/resources/db/migration/V00.00.03__Add_Data.sql
@@ -0,0 +1,4 @@
+insert into test(iso_code_2,iso_code_3,name,funny_name)
+values
+('GE','GEO','GEORGIA','GEORGIA'),
+('UK','UKE','UKRAINE','UKRAINE');
